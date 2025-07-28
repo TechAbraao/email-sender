385e1661c5da28d769274190e1514628aa8deabd
TechAbraao <abraao.cutielips@hotmail.com>
2025-07-28 17:10:46 -0300

    feat: initial configs with docker-compose

--- src/app/app.py
+++ src/app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from src.app.blueprints.routes.emails_routes import emails
+from celery import Celery
 
 def create_app() -> Flask:
     app = Flask(__name__)
@@ -9,4 +10,5 @@ def create_app() -> Flask:
 
 if __name__ == '__main__':
     app = create_app()
+    celery_app = Celery("email_sender", broker="amqp://guest:guest@localhost:5672//", backend="rpc://")
     app.run(debug=True) 
--- src/app/blueprints/routes/emails_routes.py
+++ src/app/blueprints/routes/emails_routes.py
@@ -7,10 +7,16 @@ controller = EmailsController()
 
 """  Route to send an e-mail """
 @emails.route('/send', methods=['POST'])
-def send_email(): return controller.send_email(request.get_json())
+def post_send_email(): return controller.send_email(request.get_json())
+
+@emails.route('/schedule', methods=['GET'])
+def get_schedule_email():
+    """ Route to get scheduled e-mails (not implemented yet) """
+    return {"message": "This feature is not implemented yet"}, 501
 
 @emails.route('/schedule', methods=['POST'])
-def schedule_email():
+def post_schedule_email():
     """ Route to schedule an e-mail (not implemented yet) """
     return {"message": "This feature is not implemented yet"}, 501
+
 
